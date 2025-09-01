3da23d1b53553f6d0a780ebdcee37907eb1ea2ef
cherent tsegaye <abenigas59@gmail.com>
2025-09-01 09:08:15 +0300

    f

--- db.js
+++ db.js
@@ -2,14 +2,21 @@
 const mysql = require("mysql2/promise");
 
 // Create the pool
+// const pool = mysql.createPool({
+//   host: "localhost", // or your DB host
+//   user: "root", // your MySQL username
+//   password: "", // your MySQL password
+//   database: "mekina_eta", // your database name
+//   waitForConnections: true,
+//   connectionLimit: 10, // adjust as needed
+//   queueLimit: 0,
+// });
+
 const pool = mysql.createPool({
-  host: "localhost", // or your DB host
-  user: "root", // your MySQL username
-  password: "", // your MySQL password
-  database: "mekina_eta", // your database name
-  waitForConnections: true,
-  connectionLimit: 10, // adjust as needed
-  queueLimit: 0,
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "mekina_eta",
 });
 
 (async () => {
