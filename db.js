6ddf879eb6bb898edd048e11ecd434ff207ecb27
cherent tsegaye <abenigas59@gmail.com>
2025-09-01 09:08:45 +0300

    f

--- db.js
+++ db.js
@@ -15,7 +15,7 @@ const mysql = require("mysql2/promise");
 const pool = mysql.createPool({
   host: "localhost",
   user: "root",
-  password: "123456",
+  password: "",
   database: "mekina_eta",
 });
 
